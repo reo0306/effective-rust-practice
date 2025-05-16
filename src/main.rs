bd4165ebc498cbe18d3efe7e4592ec151acd04c3
reo0306 <reo19840306@gmail.com>
2025-05-16 18:08:52 +0900

    update three one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+struct S {
+    field: Option<i32>,
+}
+
 fn main() {
     let amount_to_add = 3;
     let add_n = |y| {
@@ -5,4 +9,10 @@ fn main() {
     };
     let z = add_n(5);
     println!("{}", z);
+
+    let s = S { field: Some(42) };
+    match &s.field {
+        Some(i) => println!("field is {i}"),
+        None => {}
+    }
 }
