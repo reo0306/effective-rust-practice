a4c3356c2092bfc24a903a525e313b893e36e40c
reo0306 <reo19840306@gmail.com>
2025-05-18 21:50:53 +0900

    update three three

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ struct S {
     field: Option<i32>,
 }
 
+#[derive(Debug, Copy, Clone)]
+struct IanaAllocated(pub u64);
+
+fn is_iana_reserved(s: IanaAllocated) -> bool {
+    s.0 == 0 || s.0 == 65335
+}
+
 fn main() {
     let amount_to_add = 3;
     let add_n = |y| {
@@ -19,4 +26,9 @@ fn main() {
     if let Some(i) = &s.field {
         println!("field is {i}");
     }
+
+    let s = IanaAllocated(1);
+    println!("{:?} reserved? {}", s, is_iana_reserved(s));
+
+    if is_iana_reserved(42){}
 }
